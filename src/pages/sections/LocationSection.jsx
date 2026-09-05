00422218071d5cd3e693f35a9cf6b8197c973974
Mahir Siam <mahirsiam2004@users.noreply.github.com>
2026-09-05 15:48:05 +0600

    added api key

--- src/pages/sections/LocationSection.jsx
+++ src/pages/sections/LocationSection.jsx
@@ -80,10 +80,10 @@ export default function LocationSection() {
                         attributionControl={false}
                         style={{ width: '100%', height: '100%' }}
                     >
-                        {/* Light tile */}
+                        {/* OpenStreetMap tile - no API key required */}
                         <TileLayer
-                            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                            attribution='&copy; OpenStreetMap &copy; CARTO'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         />
                         {locations.map((loc, i) => (
                             <Marker
